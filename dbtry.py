c321f9e2e7cd6e2f16cf6b8263d203a706f0d555
User <surana.p@husky.neu.edu>
2016-05-01 00:05:13 -0400

    done some db integrations to functions

--- dbtry.py
+++ dbtry.py
@@ -3,16 +3,39 @@ from sqlalchemy import create_engine
 import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import ForeignKey
+from sqlalchemy import PrimaryKeyConstraint
 from sqlalchemy import Sequence
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+class DbException(Exception): 
+    def __init__(self, message = None, errors = None):
+        super(DbException, self).__init__(message)
+        self.errors = errors 
+        if not errors:
+            self.errors = {"status_code" : -1} 
+ # this sets the default and unused error code.
+        self.errors = errors                 
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 # dunno if we are going to use a config file later.
 dbpath = '/home/user/ims/bmidb.db'
-Base =  declarative_base()
+#Base =  declarative_base()
 
-
+# This function is symmetric to the way the DB classes work.
+# DB classes need to inherit Base (sqlalchemy class),
+# which is generated on runtime using
+# declarative_base().
+# We need to follow the symmetry and generate our class on runtime.
 def get_db_classes(Base):
-  class Project_BMI(Base):
+  class ProjectBMI(Base):
     __tablename__ = 'proj_to_network'
     pid = Column(Integer, Sequence('Project_BMI_seq'),\
             primary_key = True) 
@@ -26,12 +49,23 @@ def get_db_classes(Base):
 
   class Images(Base):
     __tablename__ = "Images"
-    pid = Column(Integer, ForeignKey(Project_BMI.pid))
+    pid = Column(Integer, ForeignKey('proj_to_network.pid'))
     filenum = Column(Integer, Sequence("Images_seq"),\
-            primary_key = True)
+            primary_key = True, unique = True)
     filename = Column(String(50))
+#TODO Bug : the tuple (pid, filename) is not unique.
+# Change this class to accomodate this constrain.
 
-  return Project_BMI, Images 
+  class ImageSnaps(Base):
+    __tablename__ = "Snaps"
+    filenum = Column(Integer,\
+             ForeignKey('Images.filenum'))
+    snap_name = Column(String(50))
+    __table_args__ = (PrimaryKeyConstraint(\
+                filenum, snap_name),) 
+# the primary key is filename+snapname
+
+  return ProjectBMI, Images, ImageSnaps 
 
 def is_db():
   global dbpath
@@ -56,7 +90,7 @@ def create_db(debug = False):
   if is_db():
     return False
   eg, Base = create_db_engine(getbase = True)
-  a, b = get_db_classes(Base)
+  a, b, c = get_db_classes(Base)
   try:
     Base.metadata.create_all(eg) 
   except Exception as e:
@@ -77,14 +111,35 @@ def test_create():
   print create_db(debug = True)
   
 
-def get_image_list_from_db(usr, passwd, project):
-  eg, base = create_db_engine(getbase = True) 
-  session = get_session(eg)
-  Project, Images = get_db_classes(base)
-  image_list = session.query(Project).filter_by(project_name = project)
+def get_image_list_from_db(project):
+  dbo = DbObject() ## gets the db part done. Just run queries! :)
+
+  p_entry  = dbo.session.query(dbo.Project).filter_by(project_name\
+           = project).first()
+# None as a query result implies that the project does not exist.
+  if p_entry is None: 
+    raise DbException(message = "No such Project",\
+            errors = {"status_code" : 491})
+
+  image_list = [a.filename for a in dbo.session.query(dbo.Images)\
+               .filter_by(pid = p_entry.pid).all()]
   return image_list
 
+# This Dbobject abstracts the whole sqlalchemy process of 
+# creating engine. Then the base. and then using the base to 
+# generate Database classes, and the engine to generate the session.
+# Just call the constructor and an object is created that has everything.
+# Typically you just need the self.session object to run the queries.
+# Refer get_image_list_from_db for an example
+class DbObject:
+  def __init__(self):
+    self.eg, self.base = create_db_engine(getbase = True) 
+    self.session = get_session(self.eg)
+    self.Project, self.Images, self.Snaps =\
+                       get_db_classes(self.base)
+  def destroy(self):
+    self.session.close()
 
 if __name__ == "__main__":
-  #test_create() 
-  print get_image_list_from_db("bb","cc","testname").all() 
+  test_create() 
+  print get_image_list_from_db("bmi_penultimate") 
--- operations.py
+++ operations.py
@@ -9,13 +9,14 @@ from urlparse import urlsplit
 import requests
 import json
 import time
+from dbtry import *
 #As of now a shell script that can accomodate rbd map and update.
 #Since there are no python rbd map and updates wrappers around ceph.
 class ShellScriptException(Exception): 
     def __init__(self, message = None, errors = None):
         super(ShellScriptException, self).__init__(message)
         if not errors:
-            errors = {"status_code" : 999}  # this sets the default and unused error code.
+            errors = {"status_code" : -1}  # this sets the default and unused error code.
         self.errors = errors                # we need this because we hash this and refer to error 
                                             
 class HaasException(Exception): 
@@ -24,7 +25,7 @@ class HaasException(Exception):
         self.errors = errors 
         print errors
         if not errors:
-            self.errors = {"status_code" : 999}  # this sets the default and unused error code.
+            self.errors = {"status_code" : -1}  # this sets the default and unused error code.
         self.errors = errors                 
 
 class GlobalConfig(object):
@@ -76,6 +77,9 @@ class ResponseException(object):
         self.exception = e
         if debug:
             print self.exception
+        if not hasattr(self.exception, 'errors'): #slightly hackish initializations,\
+# but necessary so that the dictionary can be initialized.
+            self.exception.errors = {"status_code" : -1}
 # Extend this dict as needed for future expceptions. this
 # ensures readeability and uniformity of errors.
         self.exception_dict = {
@@ -89,6 +93,7 @@ class ResponseException(object):
            type(ShellScriptException()) : 478,
            type(requests.exceptions.ConnectionError()) : 441,
            type(HaasException()) : self.exception.errors["status_code"], 
+           type(DbException()) : self.exception.errors["status_code"], 
                                 }
            
           
@@ -108,6 +113,8 @@ class ResponseException(object):
 
 #Provisioning the nodes for a given project using the image and snapshot name
 #given. The nodes are typically implemented using ceph.
+
+# provision : when map filename to num, create num files instead of image_name.
 def provision(node_name, img_name = "hadoopMaster.img",\
         snap_name = "HadoopMasterGoldenImage",\
         debug = False):
@@ -131,6 +138,7 @@ def provision(node_name, img_name = "hadoopMaster.img",\
 
 #This is for detach a node and removing it from iscsi
 #and destroying its image
+
 def detach_node(node_name):
     try:
         fsconfig = create_fsconfigobj()
@@ -143,7 +151,8 @@ def detach_node(node_name):
             fs_obj._remove(node_name.encode("utf-8"))
             return ret_200(True)
         elif 'already' in iscsi_output[0]:
-            raise ShellScriptException("Node already part unmapped and no image exists")
+            raise ShellScriptException("Node already"\
+                    " part unmapped and no image exists")
     except Exception as e:
         return ResponseException(e).parse_curr_err() 
     finally:
@@ -151,54 +160,142 @@ def detach_node(node_name):
 
 
 #Creates snapshot for the given image with snap_name as given name
-def create_snapshot(img_name, snap_name):
+def create_snapshot(url, usr, passwd, project, img_name, snap_name):
     try:
+        fs_done = False 
+        db_done = False 
+        if not check_auth(url, usr, passwd, project):
+            raise HaasException(errors = {"status_code" : 401})
+        dbo = DbObject() ## gets the db part done. Just run queries! :)
+        p_entry  = dbo.session.query(dbo.Project).filter_by(project_name\
+                = project).first()
+# None as a query result implies that the project does not exist.
+        if p_entry is None:
+            raise DbException(message = "No such Project",\
+                errors = {"status_code" : 491})
+        i_entry = dbo.session.query(dbo.Images).filter_by(pid = p_entry.pid).filter_by(filename = img_name).first()
+        if i_entry is None:
+            raise DbException(message = "No such Image",\
+                errors = {"status_code" : 492})
+        s_entry = dbo.session.query(dbo.Snaps).filter_by(filenum = i_entry.filenum).filter_by(snap_name = snap_name).first()
+        if s_entry is not None:
+            raise DbException(message = "Snap already exists",\
+                errors = {"status_code" : 493})
+        new_snap = dbo.Snaps(filenum = i_entry.filenum, snap_name = snap_name)       
+        dbo.session.add(new_snap)
+        dbo.session.flush()
+        db_done = True 
+        # MAPPINGS ## Since we change image_name to filenum that we get
+        # And we keep snap_name the same.
+        ## REDIFINE img_name as str(filenum) :) and we are set. 
+
+
+        # VIRTUALIZATION HAPPENS HERE.
+        user_img_name = img_name
+        img_name = str(i_entry.filenum)
+        #### REMAP DONE
+
+
+
+
         fsconfig = create_fsconfigobj() 
         fs_obj = init_fs(fsconfig)
+        
         if fs_obj.init_image(img_name):
             a = ret_200(fs_obj.snap_image(img_name, snap_name))
+            fs_done = True
+            dbo.session.commit()
+            db_commited = True
             fs_obj.tear_down()
             return a
     except Exception as e:
-        fs_obj.tear_down()
+        ## if db is not commited, but file operations are done
+        if fs_done and not db_commited:
+            fs_obj.tear_down()
+            remove_snaps(url, usr, passwd, project, snap_name, user_img_name) 
+        dbo.session.rollback()
         return ResponseException(e).parse_curr_err()
-
+    finally:
+            dbo.destroy()
 #Lists snapshot for the given image img_name 
-def list_snaps(img_name):
+#URL's have to be read from BMI config file
+def list_snaps(url, usr, passwd, project, img_name):
     try:
-        fsconfig = create_fsconfigobj() 
-        fs_obj = init_fs(fsconfig)
-        if fs_obj.init_image(img_name):
-            a = ret_200(fs_obj.list_snapshots(img_name))
-            fs_obj.tear_down()
-            return a
+        if not check_auth(url, usr, passwd, project):
+            raise HaasException(errors = {"status_code" : 401})
+        dbo = DbObject() ## gets the db part done. Just run queries! :)
+        p_entry  = dbo.session.query(dbo.Project).filter_by(project_name\
+                = project).first()
+# None as a query result implies that the project does not exist.
+        if p_entry is None:
+            raise DbException(message = "No such Project",\
+                errors = {"status_code" : 491})
+        i_entry = dbo.session.query(dbo.Images).filter_by(pid = p_entry.pid).filter_by(filename = img_name).first()
+        if i_entry is None:
+            raise DbException(message = "No such Image",\
+                errors = {"status_code" : 492})
+        s_entries = dbo.session.query(dbo.Snaps).filter_by(filenum = i_entry.filenum).all()
+        snap_list = [s_entry.snap_name for s_entry in s_entries] 
+        #fsconfig = create_fsconfigobj() 
+        #fs_obj = init_fs(fsconfig)
+        #if fs_obj.init_image(img_name):
+            #a = ret_200(fs_obj.list_snapshots(img_name))
+        a = ret_200(snap_list)
+            #fs_obj.tear_down()
+        return a
     except Exception as e:
-        fs_obj.tear_down()
+        #fs_obj.tear_down()
         return ResponseException(e).parse_curr_err()
-
+    finally:
+        dbo.destroy()
 #Removes snapshot sna_name for the given image img_name
-def remove_snaps(img_name, snap_name):
+def remove_snaps(url, usr, passwd, project, img_name, snap_name):
     try:
+        if not check_auth(url, usr, passwd, project):
+            raise HaasException(errors = {"status_code" : 401})
+        dbo = DbObject() ## gets the db part done. Just run queries! :)
+        p_entry  = dbo.session.query(dbo.Project).filter_by(project_name\
+                = project).first()
+# None as a query result implies that the project does not exist.
+        if p_entry is None:
+            raise DbException(message = "No such Project",\
+                errors = {"status_code" : 491})
+        i_entry = dbo.session.query(dbo.Images).filter_by(pid = p_entry.pid).filter_by(filename = img_name).first()
+        if i_entry is None:
+            raise DbException(message = "No such Image",\
+                errors = {"status_code" : 492})
+        s_entries = dbo.session.query(dbo.Snaps).filter_by(filenum = i_entry.filenum).all()
+        for s_entry in s_entries:
+            dbo.session.delete(s_entry)
+        dbo.session.flush() 
         fsconfig = create_fsconfigobj() 
         fs_obj = init_fs(fsconfig)
+        user_img_name = img_name
+        img_name = str(i_entry.filenum)
         if fs_obj.init_image(img_name):
             a = ret_200(fs_obj.remove_snapshots(img_name, snap_name))
+            dbo.session.commit()
             fs_obj.tear_down()
             return a
     except Exception as e:
-        fs_obj.tear_down()
+        if  "fs_obj" in locals():
+            fs_obj.tear_down()
+        if snap_name not in list_snaps(url, usr, passwd, project, user_img_name):
+            create_snapshot(url, usr, passwd, project, snap_name, user_img_name) 
+        dbo.session.rollback()
+        dbo.session.commit()
         return ResponseException(e).parse_curr_err()
+    finally:
+        dbo.destroy()
 
 
 
 #Lists the images for the project which includes the snapshot
-def list_all_images(debug = False):
+def list_all_images(url, usr, passwd, project):
     try:
-        fsconfig = create_fsconfigobj()
-        fs_obj = init_fs(fsconfig)
-        a = ret_200(fs_obj.list_n())
-        fs_obj.tear_down()
-        return a
+        if not check_auth(url, usr, passwd, project):
+            raise HaasException(errors = {"status_code" : 401})
+        return get_image_list_from_db(project) 
     except Exception as e:
         return ResponseException(e).parse_curr_err()
 
@@ -312,7 +409,6 @@ def attach_node_to_project_network(haas_url, node, nic,\
     ret_obj = list()
     api = '/node/' + node + '/nic/' + nic + '/connect_network'
     c_api = urlparse.urljoin(haas_url, api)
-    print c_api
     body = {"network" : network, "channel" : channel}
     haas_req = HaasRequest('post', body, auth = (usr, passwd))
     t_ret= call_haas(c_api, haas_req, debug)
@@ -368,8 +464,24 @@ def attach_node_to_project_network(haas_url, node, nic,\
     except Exception as e:
         return ResponseException(e).parse_curr_err()
     
+def check_auth(haas_url, usr, passwd, project):
+    api = '/nodes'
+    c_api = urlparse.urljoin(haas_url, '/project/' + project +  api)
+    haas_req = HaasRequest('get', None, auth = (usr, passwd))
+    ret = call_haas(c_api, haas_req)
+    if ret.status_code == 401:
+        return False
+    return True 
+
 
 if __name__ == "__main__":
+    print check_auth('http://127.0.0.1:6500/', "haasadmin", "admin1234", 'bmi_penultimate')
+    print list_all_images('http://127.0.0.1:6500/', "haasadmin", "admin1234", 'test')
+    print list_all_images('http://127.0.0.1:6500/', "haasadmin", "admin1234", 'asdfsadf')
+    print list_snaps('http://127.0.0.1:6500/', "haasadmin", "admin1234", 'bmi_penultimate','testimage' )
+    #print create_snapshot('http://127.0.0.1:6500/', "haasadmin", "admin1234", 'bmi_penultimate','testimage', 'blblb1')
+    print remove_snaps('http://127.0.0.1:6500/', "haasadmin", "admin1234", 'bmi_penultimate','testimage', 'blblb1')
+    '''
     print list_free_nodes('http://127.0.0.1:6500/', "haasadmin", "admin1234",  debug = True)['retval']
     time.sleep(5)
 
@@ -379,7 +491,7 @@ if __name__ == "__main__":
     
     print query_project_nodes('http://127.0.0.1:6500/',  "bmi_penultimate", "haasadmin", "admin1234")
     time.sleep(5)
-    '''
+    
     print attach_node_to_project_network('http://127.0.0.1:7000/', 'cisco-27',\
             "enp130s0f0", "bmi-provision","test", "test",  debug = True)
     time.sleep(5)
@@ -389,7 +501,7 @@ if __name__ == "__main__":
             'bmi-provision', "test", "test", "enp130s0f0", debug = True)
     time.sleep(5)
 
-    '''
+    
     print "above is detach from net"
     print detach_node_from_project('http://127.0.0.1:6500/',\
               "bmi_penultimate", 'sun-12',  usr = "haasadmin", passwd = "admin1234",  debug = True)
@@ -401,4 +513,4 @@ if __name__ == "__main__":
         raise ShellScriptException("lljl")
     except Exception as e:
         print ResponseException(e).parse_curr_err()
-    
+    ''' 
